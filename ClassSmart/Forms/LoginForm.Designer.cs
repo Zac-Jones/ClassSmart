20f96e347d68d87f6ee953bf63a90a4caf59758f
zojie16@gmail.com <zojie16@gmail.com>
2024-10-15 13:53:20 +1100

    Added UI elements to LoginForm

--- ClassSmart/Forms/LoginForm.Designer.cs
+++ ClassSmart/Forms/LoginForm.Designer.cs
@@ -2,15 +2,11 @@
 {
     partial class LoginForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +16,38 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginForm";
+            this.ClientSize = new System.Drawing.Size(400, 350);
+            this.Text = "Login";
+
+            this.txtUsername = new TextBox();
+            this.txtUsername.Location = new Point(100, 100);
+            this.txtUsername.Size = new Size(200, 30);
+            this.txtUsername.PlaceholderText = "Username";
+
+            this.txtPassword = new TextBox();
+            this.txtPassword.Location = new Point(100, 150);
+            this.txtPassword.Size = new Size(200, 30);
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.PlaceholderText = "Password";
+
+            this.btnLogin = new Button();
+            this.btnLogin.Location = new Point(150, 200);
+            this.btnLogin.Size = new Size(100, 30);
+            this.btnLogin.Text = "Login";
+            this.btnLogin.Click += new EventHandler(this.BtnLogin_Click);
+
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnLogin);
         }
 
-        #endregion
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            // Do login logic here
+        }
     }
 }
--- ClassSmart/Models/User.cs
+++ ClassSmart/Models/User.cs
@@ -8,5 +8,10 @@ namespace ClassSmart.Model
 {
     internal abstract class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
 }
